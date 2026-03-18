c3194d8b5068f6d2ea06ff7d959f69ddd8ae7dfc
Ashutosh Tewari <ashutoshtiwari08122001@gmail.com>
2026-03-19 03:14:45 +0530

    Add JavaScript Course

---
+++ Javascript-Course/script/11-todo-list.js
@@ -0,0 +1,48 @@
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2024-06-10",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2024-06-11",
+  },
+];
+renderTodoList();
+function renderTodoList() {
+  let todoListHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+      <button onclick="
+      todoList.splice(${i},1);
+      renderTodoList();
+      "class="delete-todo-button">Delete</button> 
+    `;
+    todoListHTML += html;
+  }
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+document.querySelector(".js-add-todo-button").addEventListener("click", () => {
+  addTodo();
+});
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+  const dateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputElement.value;
+  todoList.push({
+    name: name,
+    dueDate: dueDate,
+  });
+
+  inputElement.value = "";
+  renderTodoList();
+}
